2ed1ea8ac1dc399d02eaf13bc4a8bec1f5fb8221
HUYENH Maxime <maxime.huyenh@insa-rouen.fr>
2025-04-29 14:01:44 +0200

    Premiere version

---
+++ Classe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    class Classe
+    {
+        public List<Eleve> eleves = new List<Eleve>();
+        public List<string> matieres = new List<string>();
+        public string nomClasse;
+        public Classe(string nom)
+        {
+            nomClasse = nom;
+        }
+
+        public void ajouterEleve(string nom, string prenom)
+        {
+            var eleve = new Eleve(nom, prenom);
+            eleves.Add(eleve);
+        }
+
+        public void ajouterMatiere(string nomMatiere)
+        {
+            matieres.Add(nomMatiere);
+        }
+
+        public float moyenneMatiere(int matiereId)
+        {
+            float somme = 0;
+            foreach (var eleve in eleves)
+            {
+                somme += eleve.moyenneMatiere(matiereId);
+            }
+            try
+            {
+                return (float) Math.Round(somme / eleves.Count, 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return 0f;
+
+            }
+        }
+
+        public float moyenneGeneral()
+        {
+            float somme = 0;
+            foreach (var eleve in eleves)
+            {
+                somme += eleve.moyenneGeneral();
+            }
+            try
+            {
+                return (float)Math.Round(somme / eleves.Count, 2);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+                return 0f;
+            }
+            
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    class Eleve
+    {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public List<Note> Notes = new List<Note>();
+
+        public Eleve(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        public void ajouterNote(Note note)
+        {
+            Notes.Add(note);
+        }
+
+        public float moyenneMatiere(int matiereId)
+        {
+            float moyenne = 0;
+            int count = 0;
+
+            foreach (var note in Notes)
+            {
+              if (note.matiere == matiereId)
+                {
+                    moyenne += note.note;
+                    count++;
+                }  
+            }
+
+            try {
+                return (float)Math.Round((moyenne / count), 2);
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return 0f;
+            }
+
+            
+        }
+
+        public float moyenneGeneral()
+        {
+            float moyenne = 0;
+            Dictionary<int, float[]> moyenneParMatiere = new Dictionary<int, float[]>();
+
+            foreach (var note in Notes)
+            {
+                if (moyenneParMatiere.ContainsKey(note.matiere))
+                {
+                    moyenneParMatiere[note.matiere][0] += note.note;
+                    moyenneParMatiere[note.matiere][1]++;
+                } else
+                {
+                    moyenneParMatiere.Add(note.matiere, new float[] { note.note, 1 });
+                }
+            }
+
+            foreach (var moyenneMatiere in moyenneParMatiere)
+            {
+                moyenne += moyenneMatiere.Value[0] / moyenneMatiere.Value[1];
+            }
+            try
+            {
+                return (float)Math.Round((moyenne / moyenneParMatiere.Count), 2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return 0f;
+            }
+           
+        }
+
+    }
+}
